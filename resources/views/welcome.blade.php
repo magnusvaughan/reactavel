d985e0c943d2ae0a7eb3821cf5fcc9c12434043d
mvaughan <magnus.vaughan@gmail.com>
2018-08-24 22:52:04 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">Initial </div>
+                <div class="col-md-6">Commit</div>
+            </div>
+        </div>
+    </body>
+</html>
