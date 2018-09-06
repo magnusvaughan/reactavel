6e64b9d44083273f2f6760798aaf693d31bd6cf4
mvaughan <magnus.vaughan@gmail.com>
2018-09-06 20:56:20 +0100

    Added template landing page

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
-    <div className='container'>
-      <Link className='navbar-brand' to='/'>Mag</Link>
-    </div>
-  </nav>
-)
+    <nav className="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div className="container">{/* <Link className='navbar-brand' to='/'>Mag</Link> */}</div>
+    </nav>
+);
 
 export default Header;
--- resources/assets/js/components/Home.js
+++ resources/assets/js/components/Home.js
@@ -17,7 +17,34 @@ class Home extends Component {
             <div className="container py-4">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
-                        <h1>Testing</h1>
+                        <p>
+                            I'm{' '}
+                            <a href="https://twitter.com/magnusvaughan" target="_blank">
+                                Magnus
+                            </a>
+                        </p>
+                        <p>
+                            I'm a Software Developer at{' '}
+                            <a href="https://www.framestore.com/" target="_blank">
+                                Framestore
+                            </a>
+                            .
+                        </p>
+                        <p>I use:</p>
+                        <li>
+                            PHP
+                            <ul>
+                                <li>Laravel</li>
+                                <li>Drupal</li>
+                            </ul>
+                        </li>
+                        <li>
+                            JavaScript
+                            <ul>
+                                <li>React</li>
+                                <li>AngularJS</li>
+                            </ul>
+                        </li>
                     </div>
                 </div>
             </div>
